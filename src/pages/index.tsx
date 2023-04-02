dc815917e120d36ef3fc9affa17a43b5d451970c
Marexo <mr.marexo@gmail.com>
2023-04-02 18:41:55 +0200

    Add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -7,6 +8,9 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
+  const user = useUser();
+  console.log("USER", user.user);
+
   return (
     <>
       <Head>
@@ -15,6 +19,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        {user.isSignedIn ? (
+          <div className="flex flex-row gap-2 text-lg text-zinc-50">
+            <div>Welcome, {user.user.firstName}</div>
+            <div>|</div>
+            <SignOutButton />
+          </div>
+        ) : (
+          <SignInButton mode="modal">
+            <button className="btn">Sign in</button>
+          </SignInButton>
+        )}
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
