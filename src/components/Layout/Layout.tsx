9bb49eba4f9256cf1aa646314ed17c7405883392
Marexo <mr.marexo@gmail.com>
2023-04-10 22:12:13 +0200

    fix nav

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,19 +8,14 @@ const Layout: ReactFCC = ({ children }) => {
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      {/* <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#f2f1ef] to-[#ffffff]"> */}
       <div className="position: fixed my-2 flex w-full flex-row justify-between px-20">
-        <div className="flex flex-row gap-8">
-          <nav className="font-extrabold">
+        <nav className="flex flex-row gap-8">
+          <div className="font-extrabold">
             <Link href="/">League</Link>
-          </nav>
-          <nav>
-            <Link href="/results">My results</Link>
-          </nav>
-          <nav>
-            <Link href="/scoreboard">Scoreboard</Link>
-          </nav>
-        </div>
+          </div>
+          <Link href="/results">My results</Link>
+          <Link href="/scoreboard">Scoreboard</Link>
+        </nav>
         {data?.user?.name ? (
           <div className="flex flex-row gap-2">
             <div>
@@ -42,23 +37,6 @@ const Layout: ReactFCC = ({ children }) => {
             Sign in
           </button>
         )}
-        {/* {user.isSignedIn ? (
-          <div className="flex flex-row gap-2">
-            <div>
-              Welcome, <span className="font-bold">{user.user.firstName}</span>
-            </div>
-            <div>|</div>
-            <div className="transition duration-300 hover:text-pink-700">
-              <SignOutButton />
-            </div>
-          </div>
-        ) : (
-          <SignInButton mode="modal">
-            <button className="transition duration-300 hover:text-pink-700">
-              Sign in
-            </button>
-          </SignInButton>
-        )} */}
       </div>
       <div className="container flex flex-col items-center justify-center px-4 py-16 ">
         {children}
